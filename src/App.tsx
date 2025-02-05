4285f28dfb1859b183056ecfd63f62fde1b0b920
Raúl Luis <soyrauluis@gmail.com>
2025-02-05 01:05:10 -0500

    Upgrade packages, change theme logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@ import TypingApp from '@/components/TypingApp';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Providers from './components/Providers';
+import HideOnSmallHeight from './components/HideOnSmallHeight';
+
 import { useAtomValue } from 'jotai';
 import { themeAtom } from './atoms/ui';
 
+import '../index.css';
 import '@/assets/fonts.css';
 import '@/assets/themes/themes.css';
-import '../index.css';
-import HideOnSmallHeight from './components/HideOnSmallHeight';
+import './App.css';
+
+import { useEffect } from 'react';
 
 function App() {
     const theme = useAtomValue(themeAtom);
 
+    useEffect(() => {
+        document.documentElement.classList.add(theme);
+
+        return () => {
+            document.documentElement.classList.remove(theme);
+        };
+    }, [theme]);
+
     return (
         <Providers>
-            <div className={theme + ' antialiased'}>
+            <div className={'antialiased bg-background text-text'}>
                 <div className='min-h-svh transition-colors duration-150 ease-out'>
                     <div className='max-w-screen-xl xl:mx-auto xl:container content-grid layout-grid'>
                         <div className='row-start-[top-start] row-end-[content-start] col-[content]'>
